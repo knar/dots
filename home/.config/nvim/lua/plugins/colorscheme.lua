5f7acf8ceb35ecc03742ea92f2b64a8038a11d9e
knar <zakjsimpson@gmail.com>
2024-02-14 21:31:57 -0500

    Use rustaceanvim

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,28 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none"
-            }
-          }
-        }
-      }
-    }
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
-  },
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {
+			keywordStyle = {
+				italic = false,
+			},
+			statementStyle = {
+				bold = false,
+			},
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "kanagawa",
+		},
+	},
 }
