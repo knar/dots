419fb62c4fe19a1402adab435cf8c32995e15370
knar <zakjsimpson@gmail.com>
2024-02-14 21:12:42 -0500

    aslfdj

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,10 @@
 abbr -a vim nvim
 abbr -a vi nvim
 
+source ~/.config/fish/kanagawa.fish
+
 if status is-login
     if test (tty) = /dev/tty1
-        exec Hyprland
+        exec Hyprland &>~/.logs/hyprland
     end
 end
