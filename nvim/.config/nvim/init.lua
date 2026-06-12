2b0a00ba35541c839aa17feb48c7279ab61b7bd0
Zak Simpson <zakjsimpson@gmail.com>
2026-06-12 15:06:26 -0400

    set theme properly and tabs->spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,38 +7,38 @@ vim.o.winborder = "rounded"
 vim.o.scrolloff = 10
 
 vim.diagnostic.config({
-	severity_sort = true,
-	jump = { on_jump = true, wrap = true },
-	virtual_text = { prefix = "●" },
-	signs = { text = { " ", " ", " ", " " } },
+    severity_sort = true,
+    jump = { on_jump = true, wrap = true },
+    virtual_text = { prefix = "●" },
+    signs = { text = { " ", " ", " ", " " } },
 })
 
 vim.pack.add({
-	"https://github.com/max397574/better-escape.nvim",
-	"https://github.com/echasnovski/mini.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", build = ":TSUpdate" },
-	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("*") },
-	"https://github.com/stevearc/conform.nvim",
-	"https://github.com/j-hui/fidget.nvim",
-	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
-	"https://github.com/brenoprata10/nvim-highlight-colors",
-	"https://github.com/RRethy/vim-illuminate",
-	"https://github.com/christoomey/vim-tmux-navigator",
-	"https://github.com/rebelot/kanagawa.nvim",
-	"https://github.com/maxmx03/solarized.nvim",
-	"https://github.com/ellisonleao/gruvbox.nvim",
+    "https://github.com/max397574/better-escape.nvim",
+    "https://github.com/echasnovski/mini.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", build = ":TSUpdate" },
+    { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("*") },
+    "https://github.com/stevearc/conform.nvim",
+    "https://github.com/j-hui/fidget.nvim",
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+    "https://github.com/brenoprata10/nvim-highlight-colors",
+    "https://github.com/RRethy/vim-illuminate",
+    "https://github.com/christoomey/vim-tmux-navigator",
+    "https://github.com/rebelot/kanagawa.nvim",
+    "https://github.com/maxmx03/solarized.nvim",
+    "https://github.com/ellisonleao/gruvbox.nvim",
 })
 
 require("better_escape").setup({})
 
 require("mini.basics").setup({ options = { extra_ui = true } })
 require("mini.clue").setup({
-	triggers = {
-		{ mode = { "n", "x" }, keys = "<Leader>" },
-		{ mode = { "n", "x" }, keys = "g" },
-		{ mode = "n", keys = "<C-w>" },
-	},
+    triggers = {
+        { mode = { "n", "x" }, keys = "<Leader>" },
+        { mode = { "n", "x" }, keys = "g" },
+        { mode = "n", keys = "<C-w>" },
+    },
 })
 require("mini.diff").setup({})
 require("mini.files").setup({})
@@ -50,26 +50,26 @@ require("mini.statusline").setup({})
 require("mini.surround").setup({})
 
 require("blink.cmp").setup({
-	keymap = {
-		preset = "enter",
-		["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-		["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-		["<Escape>"] = { "hide", "fallback" },
-	},
-	completion = {
-		list = { selection = { preselect = false, auto_insert = true } },
-	},
+    keymap = {
+        preset = "enter",
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<Escape>"] = { "hide", "fallback" },
+    },
+    completion = {
+        list = { selection = { preselect = false, auto_insert = true } },
+    },
 })
 
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		rust = { "rustfmt" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+    formatters_by_ft = {
+        lua = { "stylua" },
+        rust = { "rustfmt" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+    },
 })
 
 require("fidget").setup({})
@@ -77,46 +77,47 @@ require("fidget").setup({})
 require("nvim-highlight-colors").setup({})
 
 require("kanagawa").setup({
-	theme = "dragon",
-	background = { dark = "dragon" },
-	keywordStyle = { italic = false },
-	colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-	overrides = function(colors)
-		local theme = colors.theme
-		local blend_bg = function(color)
-			local c = require("kanagawa.lib.color")
-			return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-		end
-		return {
-			Pmenu = { link = "NormalFloat", blend = vim.o.pumblend },
-			PmenuThumb = { bg = theme.ui.float.fg_border },
-			BlinkCmpMenuBorder = { link = "FloatBorder" },
-			BlinkCmpMenuSelection = { bg = theme.ui.bg_p1 },
-			BlinkCmpLabelDetail = { link = "NormalFloat" },
-			BlinkCmpLabelDescription = { link = "NormalFloat" },
-			DiagnosticVirtualTextHint = blend_bg(theme.diag.hint),
-			DiagnosticVirtualTextInfo = blend_bg(theme.diag.info),
-			DiagnosticVirtualTextWarn = blend_bg(theme.diag.warning),
-			DiagnosticVirtualTextError = blend_bg(theme.diag.error),
-			IlluminatedWordText = { link = "LspReferenceText" },
-			IlluminatedWordRead = { link = "LspReferenceRead" },
-			IlluminatedWordWrite = { link = "LspReferenceWrite" },
-		}
-	end,
+    theme = "dragon",
+    background = { dark = "dragon" },
+    keywordStyle = { italic = false },
+    colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+    overrides = function(colors)
+        local theme = colors.theme
+        local blend_bg = function(color)
+            local c = require("kanagawa.lib.color")
+            return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
+        return {
+            Pmenu = { link = "NormalFloat", blend = vim.o.pumblend },
+            PmenuThumb = { bg = theme.ui.float.fg_border },
+            BlinkCmpMenuBorder = { link = "FloatBorder" },
+            BlinkCmpMenuSelection = { bg = theme.ui.bg_p1 },
+            BlinkCmpLabelDetail = { link = "NormalFloat" },
+            BlinkCmpLabelDescription = { link = "NormalFloat" },
+            DiagnosticVirtualTextHint = blend_bg(theme.diag.hint),
+            DiagnosticVirtualTextInfo = blend_bg(theme.diag.info),
+            DiagnosticVirtualTextWarn = blend_bg(theme.diag.warning),
+            DiagnosticVirtualTextError = blend_bg(theme.diag.error),
+            IlluminatedWordText = { link = "LspReferenceText" },
+            IlluminatedWordRead = { link = "LspReferenceRead" },
+            IlluminatedWordWrite = { link = "LspReferenceWrite" },
+        }
+    end,
 })
 
 -- theme
+local function set_theme()
+    if vim.o.background == "dark" then
+        vim.cmd("colorscheme kanagawa")
+    else
+        -- require("gruvbox").setup({ contrast = "soft" })
+        vim.cmd("colorscheme gruvbox")
+    end
+end
+set_theme()
 vim.api.nvim_create_autocmd("OptionSet", {
-	pattern = "background",
-	callback = function()
-		if vim.o.background == "dark" then
-			vim.cmd("colorscheme kanagawa")
-		else
-			-- require("gruvbox").setup({ contrast = "soft" })
-			vim.cmd("colorscheme gruvbox")
-			-- vim.cmd("colorscheme solarized")
-		end
-	end,
+    pattern = "background",
+    callback = set_theme,
 })
 
 -- keymaps
@@ -132,36 +133,36 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagn
 
 vim.keymap.set("n", "<leader>qq", vim.diagnostic.setqflist, { desc = "Show quickfix list" })
 vim.keymap.set("n", "<leader>qe", function()
-	vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR })
+    vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR })
 end, { desc = "Show quickfix list (errors)" })
 
 vim.keymap.set("n", "<leader>e", function()
-	local buf_name = vim.api.nvim_buf_get_name(0)
-	local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
-	MiniFiles.open(path)
+    local buf_name = vim.api.nvim_buf_get_name(0)
+    local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+    MiniFiles.open(path)
 end, { desc = "Open file explorer" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function(event)
-		local opts = { buffer = event.buf }
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set("n", "<C-.>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	end,
+    desc = "LSP actions",
+    callback = function(event)
+        local opts = { buffer = event.buf }
+        vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        vim.keymap.set("n", "<C-.>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    end,
 })
 
 -- LSPs
 local servers = { "rust_analyzer", "openscad_lsp" }
 
 vim.lsp.config("rust_analyzer", {
-	settings = {
-		["rust-analyzer"] = {
-			cargo = { features = "all" },
-			check = { command = "clippy" },
-		},
-	},
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = { features = "all" },
+            check = { command = "clippy" },
+        },
+    },
 })
 
 for _, server in ipairs(servers) do
-	vim.lsp.enable(server)
+    vim.lsp.enable(server)
 end
